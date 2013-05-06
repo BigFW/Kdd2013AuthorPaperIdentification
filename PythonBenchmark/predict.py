24cb7a924aa1f280fa331265f620fd2db9047197
Big FW <yusiyu555@sina.cn>
2013-05-06 15:12:18 +0800

    123
    123

--- PythonBenchmark/predict.py
+++ PythonBenchmark/predict.py
@@ -10,7 +10,7 @@ def main():
     print("Loading the classifier")
     classifier = data_io.load_model()
 
-    print("Making predictions")
+    print("Making predictions")i
     predictions = classifier.predict_proba(features)[:,1]
     predictions = list(predictions)
 
